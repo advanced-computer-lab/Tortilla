8b19ebb0a4f3fb69a2c48748e42dc715e2f27c94
Intern07 <Intern07@Qcentriseg.com>
2021-11-30 23:26:30 +0200

    css

--- client/src/Component/Administrator/Administrator.js
+++ client/src/Component/Administrator/Administrator.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './update.css';
+//import './update.css';
 
 function Administrator() {
     const [FlightNumberUpdate, setFlightNumberUpdate] = useState();
